b2957a76a14a070c462d8d08b5d023d1ad51ecf2
henriqueCostaSI <henrique.mcosta@hotmail.com>
2022-09-28 19:18:47 -0300

    reconhecimento

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,3 +1,5 @@
+const elementoChute = document.getElementById('chute');
+
 const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
@@ -9,5 +11,15 @@ recognition.start();
 recognition.addEventListener('result', onSpeak);
 
 function onSpeak(evento) {
-    console.log(evento.results[0][0].transcript);
-}
+    chute = evento.results[0][0].transcript;
+    exibeChuteNaTela(chute);
+};
+
+exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>  
+        <span class="box">${chute}</span>
+    `
+        
+        
+};
